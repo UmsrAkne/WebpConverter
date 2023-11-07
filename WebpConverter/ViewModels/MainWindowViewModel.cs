27c29a1ca587f7763216e1b483a79e59be5c70b2
Akn <sibata486@gmail.com>
2023-11-08 00:10:47 +0900

    add / MainWindow がドラッグアンドドロップを受け付けるようにした

--- WebpConverter/ViewModels/MainWindowViewModel.cs
+++ WebpConverter/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using Prism.Commands;
 using Prism.Mvvm;
 using WebpConverter.Models;
@@ -8,10 +10,16 @@ namespace WebpConverter.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
+        private List<ExFileInfo> webpFiles;
+
         public string Title => "webp converter";
 
         // DragAndDropBehavior からデータが入力される。
-        public List<ExFileInfo> WebpFiles { get; set; }
+        public List<ExFileInfo> WebpFiles
+        {
+            get => webpFiles;
+            set => SetProperty(ref webpFiles, value);
+        }
 
         public DelegateCommand ConvertWebpToPngCommand => new DelegateCommand(() =>
         {
