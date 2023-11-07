80128aa8cc79dc91565aefb3fed56f12db73aaaf
Akn <sibata486@gmail.com>
2023-11-07 22:45:26 +0900

    init

---
+++ WebpConverter/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using WebpConverter.Views;
+
+namespace WebpConverter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ WebpConverter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Mvvm;
+
+namespace WebpConverter.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title => "webp converter";
+    }
+}
