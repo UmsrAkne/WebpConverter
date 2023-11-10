7027ed9c69102265e5307d91c8cab3535015b526
Akn <sibata486@gmail.com>
2023-11-10 21:50:53 +0900

    add feat / CommandGen.GetCommand に出力先を指定するオーバーロードを追加

--- WebpConverter/Models/CommandGen.cs
+++ WebpConverter/Models/CommandGen.cs
@@ -12,5 +12,14 @@ namespace WebpConverter.Models
             var option = $"-o \"{exFileInfo.FileInfo.FullName}\"";
             return $"{option} \"{inputFileInfo.FullName}\"";
         }
+
+        public static string GetCommand(FileInfo inputFileInfo, DirectoryInfo outputDirectoryInfo)
+        {
+            var exFileInfo = new ExFileInfo(inputFileInfo);
+            exFileInfo.SetExtension(".png");
+
+            var option = $"-o \"{outputDirectoryInfo.FullName}\\{exFileInfo.FileInfo.Name}\"";
+            return $"{option} \"{inputFileInfo.FullName}\"";
+        }
     }
 }
